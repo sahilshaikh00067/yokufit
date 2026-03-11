89ee45b6a593bbbe276705b717f0e31cfb34ff1f
Sahil Shaikh <sahil@gmail.com>
2026-03-11 17:13:05 +0530

    update settings for media

--- yogabackend/urls.py
+++ yogabackend/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # API routes
+
+    # API routes
+    path('api/', include('api.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# MEDIA FILES (images upload)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
